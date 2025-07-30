721294545d4a21c9308ff74a9d8988ca50712b23
UdaraAthauda <athaudaprasath@gmail.com>
2025-07-30 17:52:27 +0530

    basic data rendering from backend to frontend using api

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AxiosInstance from './Axios'
 
 const Create = () => {
+
+  const [country, setCountry] = useState([]);
+  const [league, setLeague] = useState([]);
+  const [characteristic, setCharacteristic] = useState([]);
+
+  console.log(country, league, characteristic)
+
+  const GetData = () => {
+    AxiosInstance.get(`country/`).then((res) => setCountry(res.data)).catch((error) => alert(error));
+    
+    AxiosInstance.get(`league/`).then((res) => setLeague(res.data)).catch((error) => alert(error));
+    
+    AxiosInstance.get(`characteristic/`).then((res) => setCharacteristic(res.data)).catch((error) => alert(error));
+  }
+
+  useEffect(() => {
+    GetData()
+  }, [])
+
   return (
     <div>Create</div>
   )
