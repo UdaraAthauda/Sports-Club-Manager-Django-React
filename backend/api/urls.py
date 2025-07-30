12e05828abf1706fc04c16d2376313223eaab608
UdaraAthauda <athaudaprasath@gmail.com>
2025-07-30 17:04:16 +0530

    created serializers,views and api urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register('country', CountryViewset, basename="country")
+router.register('league', LeagueViewset, basename="league")
+router.register('characteristic', CharacteristicViewset, basename="characteristic")
+
+urlpatterns = router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class CountryViewset(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    
+    def list(self, request):
+        queryset = Country.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+
+class LeagueViewset(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+    
+    def list(self, request):
+        queryset = League.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+
+class CharacteristicViewset(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+    queryset = Characteristic.objects.all()
+    serializer_class = CharacteristicSerializer
+    
+    def list(self, request):
+        queryset = Characteristic.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
