8801db99497ffdd2891e775c7373334220ed3ee8
UdaraAthauda <athaudaprasath@gmail.com>
2025-07-30 16:21:55 +0530

    database models creation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class League(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Characteristic(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class FootBallClub(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+    attendence = models.IntegerField(null=True)
+    city = models.CharField(max_length=50)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    league = models.ForeignKey(League,on_delete=models.CASCADE)
+    characteristic = models.ManyToManyField(Characteristic)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
