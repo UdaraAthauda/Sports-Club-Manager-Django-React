ef7e14e852aab694e6b0a0d849871f68d22d95d8
UdaraAthauda <athaudaprasath@gmail.com>
2025-07-31 10:43:05 +0530

    implemented the responsive data creation form

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import AxiosInstance from './Axios'
+import {Box , Typography} from '@mui/material'
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import TextForm from '../forms/TextForm';
+import SelectForm from '../forms/SelectForm';
+import MultiSelectForm from '../forms/MultiSelectForm';
+import DescriptionForm from '../forms/DescriptionForm';
+import Button from '@mui/material/Button';
 
 const Create = () => {
 
@@ -7,7 +14,6 @@ const Create = () => {
   const [league, setLeague] = useState([]);
   const [characteristic, setCharacteristic] = useState([]);
 
-  console.log(country, league, characteristic)
 
   const GetData = () => {
     AxiosInstance.get(`country/`).then((res) => setCountry(res.data)).catch((error) => alert(error));
@@ -22,7 +28,49 @@ const Create = () => {
   }, [])
 
   return (
-    <div>Create</div>
+    <>
+      <Box className='TopBar'>
+        <AddBoxIcon />
+        <Typography sx={{marginLeft: '15px', fontWeight: 'bold'}} variant='subtitle2'>Create a new club</Typography>
+      </Box>
+
+      <Box className='FormBox'>
+        <Box className='FormArea'>
+        
+          <TextForm label={'Club name'} />
+   
+          <Box sx={{marginTop: '30px'}}>
+            <TextForm label={'City'} />
+          </Box>
+          
+          <Box sx={{marginTop: '30px'}}>
+            <SelectForm label={'League'} options={league} />
+          </Box>
+
+          <Box sx={{marginTop: '30px'}}>
+            <Button variant="contained" fullWidth>Submit the data</Button>
+          </Box>
+        </Box>
+
+
+        <Box className='FormArea'>
+          <SelectForm label={'Country'} options={country} />
+
+          <Box sx={{marginTop: '30px'}}>
+            <TextForm label={'Attendence'} />
+          </Box>
+          
+          <Box sx={{marginTop: '30px'}}>
+            <MultiSelectForm label={'Characteristic'} options={characteristic} />
+          </Box>
+        </Box>
+
+
+        <Box className='FormArea'>
+          <DescriptionForm label={'Description'} rows={9} />
+        </Box>
+      </Box>
+    </>
   )
 }
 
---
+++ frontend/src/forms/TextForm.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+export default function TextForm({label}) {
+  return (
+
+      <TextField id="outlined-basic" label={label} variant="outlined" sx={{width: '100%'}} />
+  
+  );
+}
