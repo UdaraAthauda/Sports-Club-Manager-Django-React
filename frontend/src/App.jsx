4408b482eeaa859d96a91b73aea1214bb9f6625e
UdaraAthauda <athaudaprasath@gmail.com>
2025-08-01 11:39:41 +0530

    update in navigation menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Create from './components/Create'
 import Edit from './components/Edit'
 import Delete from './components/Delete'
 import Navbar from './components/navbar/Navbar'
+import ClubDetails from './components/ClubDetails'
 import './App.css'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path='/create' element={<Create />} />
           <Route path='/edit/:id' element={<Edit />} /> {/* passing parameters '/:id' */}
           <Route path='/delete/:id' element={<Delete />} />
+          <Route path='/details/:id' element={<ClubDetails />} />
         </Routes>
       } />
       
--- frontend/src/components/navbar/Menu.jsx
+++ frontend/src/components/navbar/Menu.jsx
@@ -1,82 +1,101 @@
-import * as React from 'react';
-import ListSubheader from '@mui/material/ListSubheader';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import {Link, useLocation} from 'react-router-dom'
-
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Link, useLocation } from "react-router-dom";
+import AxiosInstance from "../Axios";
 
 export default function Menu() {
   const [open, setOpen] = React.useState(true);
+  const [clubs, setClubs] = React.useState([]);
+
+  const GetData = () => {
+    AxiosInstance.get(`country/`)
+      .then((res) => setClubs(res.data));
+  };
+
+  React.useEffect(() => {
+    GetData();
+  }, []);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  const location = useLocation()
-  const path = location.pathname
+  const location = useLocation();
+  const path = location.pathname;
 
   return (
-
     <>
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Football clubs
+          </ListSubheader>
+        }
+      >
+        <ListItemButton
+          onClick={handleClick}
+          component={Link}
+          to="/"
+          selected={path === "/"}
+        >
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="All clubs" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
 
-    <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Football clubs
-        </ListSubheader>
-      }
-    >
-   
-      <ListItemButton onClick={handleClick} component={Link} to="/" selected={path === "/"}>
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="All clubs" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <DashboardCustomizeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Lanka" />
-          </ListItemButton>
-        </List>
-      </Collapse>
-    </List>
+          {clubs.map((club, index) => {
+            return (
+            <List component="div" disablePadding key={club.id}>
+              <ListItemButton sx={{ pl: 4 }} component={Link} to={`details/${club.id}`}>
+                <ListItemIcon>
+                  <DashboardCustomizeIcon />
+                </ListItemIcon>
+                <ListItemText primary={club.name} />
+              </ListItemButton>
+            </List>
+            )
+          })}
 
+        </Collapse>
+      </List>
 
-    <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Creating Records
-        </ListSubheader>
-      }
-    >
-   
-      <ListItemButton component={Link} to="/create" selected={path === "/create"}>
-        <ListItemIcon>
-          <AddBoxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Create Clubs" />
-      </ListItemButton>
-    </List>
-
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Creating Records
+          </ListSubheader>
+        }
+      >
+        <ListItemButton
+          component={Link}
+          to="/create"
+          selected={path === "/create"}
+        >
+          <ListItemIcon>
+            <AddBoxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Create Clubs" />
+        </ListItemButton>
+      </List>
     </>
   );
 }
